02144782e81e4f33f3a3c8ba1d7e41177f2b9217
djgnfj-svg <djgnfj8923@naver.com>
2024-11-24 23:39:51 +0900

    add post,book page

---
+++ frontend/src/app/page.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to the Next.js Frontend</h1>
+      <p>Click the links above to explore the features.</p>
+    </div>
+  );
+}
