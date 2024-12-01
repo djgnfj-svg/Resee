c89483944e28f9e9de548ac7f4b9ab539e1ec8e8
djgnfj-svg <djgnfj8923@naver.com>
2024-12-01 13:17:04 +0900

    book model fix:

--- backend/book/models.py
+++ backend/book/models.py
@@ -8,7 +8,24 @@ class BookCategory(models.Model):
 
 # 책 모델
 class Book(TimeStampedModel):
+    # 책 표지 선택지
+    COVER_CHOICES = [
+        ('cover1', '/book_covers/cover_white.png'),
+        ('cover2', '/book_covers/cover_white.png'),
+        ('cover3', '/book_covers/cover_white.png'), 
+        ('cover4', '/book_covers/cover_white.png'),
+        ('cover5', '/book_covers/cover_white.png'),
+    ]
+
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="books")
-    category = models.ForeignKey(BookCategory, on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(max_length=20)
+    cover = models.CharField(
+        max_length=20,
+        choices=COVER_CHOICES,
+        default=COVER_CHOICES[0]
+    )
+    category = models.ForeignKey(BookCategory, on_delete=models.SET_NULL, null=True, blank=True)
     simple_explanation = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
